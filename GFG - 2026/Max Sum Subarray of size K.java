468f2d80d9616bb8f382b9ce0659b56824a38239
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2026-01-05 21:46:37 +0530

    Create Max Sum Subarray of size K.java

---
+++ GFG - 2026/Max Sum Subarray of size K.java
@@ -0,0 +1,38 @@
+Max Sum Subarray of size K
+Difficulty: EasyAccuracy: 49.6%Submissions: 237K+Points: 2
+Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
+
+Note: A subarray is a contiguous part of any given array.
+
+Examples:
+
+Input: arr[] = [100, 200, 300, 400], k = 2
+Output: 700
+Explanation: arr2 + arr3 = 700, which is maximum.
+Input: arr[] = [1, 4, 2, 10, 23, 3, 1, 0, 20], k = 4
+Output: 39
+Explanation: arr1 + arr2 + arr3 + arr4 = 39, which is maximum.
+Input: arr[] = [100, 200, 300, 400], k = 1
+Output: 400
+Explanation: arr3 = 400, which is maximum.
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ 106
+1 ≤ k ≤ arr.size()class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        int n = arr.length;
+        if (n < k) return -1;
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+        for (int i = k; i < n; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+}
